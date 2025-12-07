690d5ee6141cb1c3a36a15eff34acfecf43cd6d5
tranc43 <tranc43@macewan.ca>
2025-12-07 09:10:53 -0700

    Added header file

---
+++ src/splash.h
@@ -0,0 +1,20 @@
+/**
+@file splash.h
+@brief Function declarations for game menu screen
+
+@brief Displays main game splash screen with menu options
+menu includes
+- Game title
+- Game Description
+- Play Game Options
+- Quit game option
+- User has capability with navigating with arrow keys and pressing enter to select
+- Q to quit program
+*/
+
+#ifndef SPLASH_h
+#define SPLASH_H
+
+void splash_screen();
+
+#endif
