e66fd1dd351beb9625862a7ccb547818171fbbf2
tranc43 <tranc43@macewan.ca>
2025-12-07 16:22:42 -0700

    Working on level.c

---
+++ src/level.c
@@ -0,0 +1,11 @@
+#include <ncurses.h>
+#include <string.h>
+#include <level.h>
+
+int level_select_menu(void) {
+	const char *options[] = { "Level 1", "Level 2", "Go back"};
+	int num_options 3; 
+	
+
+
+}
