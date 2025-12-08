12abef9aa598b6791c320ad9b101b109be046b74
tranc43 <tranc43@macewan.ca>
2025-12-07 17:40:02 -0700

    Reworked splash.c

--- src/splash.c
+++ src/splash.c
@@ -1,34 +1,41 @@
 #include <ncurses.h>
 #include "splash.h"
-
+#include <string.h>
+#include "level.h"
 void splash_screen() {
 	int highlight = 0;
 	int choice;
 	const char *options[] = {"Play Game", "Quit"};
 	int num_options = 2;
 	
+	int max_y, max_x;
+	get_maxyx(stdscr, max_y, max_x);
 	while (1) { 
 		clear();
 		mvprintw(3, 25, " ==== MACMAN ====");
-	
-		mvprintw(5, 10, "Navigate through mazes!");
-		mvprintw(6, 10, "Interact with NPCS to receive a key to exit to survive!");
+		mvprintw(3, (max_x - (int)strlen(title)) / 2, "%s", title);
 		
-		mvprintw(8, 10, "Arrow keys to move, ENTER to select.");
-		mvprintw(9, 10, "Press 'Q' to quit");
-	
+		const char *line1 = "Navigate through mazes!";
+		const char *line2 = "Interact with NPCs to receieve a key to exit to survive!");
+		mvprintw(6, (max_x - (int)strlen(title)) / 2, "%s", line1);
+		mvprintw(7, (max_x - (int)strlen(title)) / 2, "%s", line2);
+		
+		conar char *instructions =  "Arrow keys to move, ENTER to select, Q to quit.";
+		mvprintw(10, (max_x - (int)strlen(instructions) / 2, "%s", instructions);
+		
+		int start_y = 14;
 		for (int i = 0; i < num_options; i++) {
+			int x= = (max_x - (int)strlen(options[i])) / 2;
 			if (i == highlight) {
 				attron(A_REVERSE);
 			}
-			mvprintw(12 + i, 10, options[i]);
+			mvprintw(start_y + i, x, options[i]);
 			attroff(A_REVERSE);
 		}
 		refresh();
 		choice = getch();
 		// Quit 
 		if (choice == 'q' || choice == 'Q') {
-			endwin();
 			return;
 		}
 		switch (choice) {
@@ -44,16 +51,17 @@ void splash_screen() {
 				break;
 			case 10: // ENTER
 				if (highlight == 0) {
-					clear();
-					mvprintw(5, 10, "Starting game.. (Level system will be here)");
-					refresh();
-					refresh();
-					getch();
-					return;
+					int selected = level_select_menu()'
+					if (selected == -1) { 
+						return;
+					} else if (select == 0) {
+						continue;
+					} else {
+						display_level(selected);
+					}
 				}
 				else if (highlight == 1) {
 					// Quit selected
-					endwin();
 					return;
 				}
 				break;
