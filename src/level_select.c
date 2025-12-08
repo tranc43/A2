88fa8b7caf038dd90a51b72a2a4e0961382e0896
tranc43 <tranc43@macewan.ca>
2025-12-07 20:18:56 -0700

    Created level select.c

---
+++ src/level_select.c
@@ -0,0 +1,58 @@
+#include <ncurses.h>
+#include <string.h>
+#include "level_select.h"
+
+static void center_text(int y, const char *text) {
+	int max_y, max_x;
+	getmaxyx(stdscr, max_y, max_x);
+	mvprintw(y, (max_x - (int)strlen(text)) / 2, "%s", text);
+
+}
+
+
+int level_select_menu(void) {
+	const char *options[] = {"Level 1", "Level 2", "Return to Menu"};
+	int num_options = 3;
+	int hightlight = 0;
+	int ch;
+	
+	while (1) {
+		clear();
+		box(stdscr,0,0);
+		
+		center_text(3, "========== SELECT LEVEL ==========");
+		center_text(6, "Use arrow keys to navigate, ENTER to select.");
+		center_text(7, "Press Q to quit.");
+
+		int start_y = 10;
+		for (int i = 0; i < num_options; i++) {
+			if (i == highlight)
+				attron(A_REVERSE);
+			center_text(start_y + i , options[i]);
+
+			if (i == highlight)
+				attroff(A_REVERSE);
+
+		}
+		refresh();
+		ch = getch();
+
+		if (ch == 'q' || ch == 'Q')
+			return -1;
+		switch (ch) {
+			case KEY_UP:
+				highlight = (highlight - 1 + num_options) % num_options;
+				break;
+			case KEY_DOWN:
+				highlight = (highlight + 1) % num_options;
+				break;
+			case 10: // ENTER
+				if (highlight == 0) return 1;
+				if (highlight == 1) return 2;
+				if (highlight == 2) return 0; 
+
+		}
+
+	}
+
+}
