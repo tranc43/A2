010862907f4b61dae4c3291d53e0d7fcf84a3c6c
tranc43 <tranc43@macewan.ca>
2025-12-09 08:46:34 -0700

    main.c comments

--- src/main.c
+++ src/main.c
@@ -1,5 +1,31 @@
+/*
+@name Christian Tran
+@file main.c
+@brief Entry point for the MACMAN game
+
+this file intiializes ncurses, launches and the splash screen 
+then after cleans up the terminal environment when the game ends.
+
+
+Main function does not run gameplay directly it only delegates.
+
+	splash_screen() -> menu
+	level_select_menu() -> choose a level
+	play_level1() / play_level2() -> gameplay loops
+
+*/
+
+
+
 #include <ncurses.h>
 #include "splash.h"
+
+
+/*
+@brief Program entry point intiailzies ncurses and run splash screen
+@return int always return 0 on clean exit.
+
+*/
 
 int main() {
 	initscr();
