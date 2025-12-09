e9b74624bd495e2c1c99688f86e122f57417808b
tranc43 <tranc43@macewan.ca>
2025-12-09 08:29:32 -0700

    splash h comments

--- src/splash.h
+++ src/splash.h
@@ -1,19 +1,28 @@
 /**
-@file splash.h
-@brief Function declarations for game menu screen
+* @file splash.h
+* @brief Function declarations for game menu screen
+*
+* Splash scrfeen is shown to user upon launch and provides:
+*- Game title
+*- Game Description
+*- Play Game Options (Leading to level selection)
+*- Quit game option
+*- User has capability with navigating with arrow keys and pressing enter to select
+*- Q to quit program
 
-@brief Displays main game splash screen with menu options
-menu includes
-- Game title
-- Game Description
-- Play Game Options
-- Quit game option
-- User has capability with navigating with arrow keys and pressing enter to select
-- Q to quit program
 */
 
 #ifndef SPLASH_h
 #define SPLASH_H
+
+
+/**
+@brief displays splash screen and handles user input, 
+- function loops interallyt until player chooses either to exit
+or play the game
+@return void
+
+*/
 
 void splash_screen();
 
