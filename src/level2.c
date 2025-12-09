5a07e5d20962475ba716e180428ba317fb1eda64
tranc43 <tranc43@macewan.ca>
2025-12-08 20:24:41 -0700

    functions

--- src/level2.c
+++ src/level2.c
@@ -42,14 +42,13 @@ typedef struct {
 	int player_y, player_x;
 	int npc_y, npc_x;
 	int npc_dir; // 1 = move right, -1 = move left
-
-	int enemy_y, enemy_x;
-	int enemy_dir; // 1 = moving down, -1 m= moving up
-
+		
 	bool has_key;
+	bool key_on_ground;
+	int key_y, key_x;
 	bool running;
 
-} level2State;
+} Level2State;
 
 static void center_text(int y, const char *text) {
 	int max_y, max_x;
@@ -85,7 +84,66 @@ static void game_end_screen(const char *line1, const char *line2) {
 	int max_y, int max_x;
 	getmaxyx(stdscr< max_y, max_x);
 
+	clear();
+	box(stdscr, 0, 0);
+	center_text(max_y / 2 - 2, line1);
+	center_text(max_y / 2 - 1, line2);
+	center_text(max_y / 2 + 1, "Press M to return main menu or Q to quit.");
+	refresh();
+
+	int ch;
+	while (1) {
+		ch = getch();
+		if (ch == 'q' || ch == 'Q') {
+			endwin();
+			exit(0);
+		}
+		if (ch == 'm' || ch == 'M') {
+			break;
+		}
+	}
 }
+
+
+
+static char level2_get_tile(int y, int x) {
+	 if (y < 0 || y >= LEVEL2_HEIGHT || x < 0 || x >= LEVEL2_WIDTH) {
+		return '#';
+	}
+	return level2_layout[y][x];
+
+}
+
+static char level2_can_move_to(int new_y, int new_x) {
+	char tile = level2_get_tile(new_y, new_x);
+	return tile != '#';
+
+}
+
+static void level2_render_map(int offset_y, int offset_x) {
+	for (int y = 0; y < LEVEL2_HEIGHT; y++) {
+		for (int x = 0; x < LEVEL2_WIDTH; x++) {
+			char tile = level2_get_tile(y, x);
+			mvaddch(offset_y + y, offset_x + x, tile);
+		}
+
+	}
+
+}
+
+static void level2_render_entitites(const Level2State *st, int offset_y, int offset_x) {
+		mvaddch(offset_y + st->npc_y, offset_x + st->npc_X, 'N');
+
+		if (st->key_on_ground && !st->has_key) {
+			mvaddch(offset_y + st->key_y, offset_x + st->key_x, 'K');
+
+		}
+
+}
+
+
+
+
 
 
 
